9c31fcd660babf505755f8ce43178fbf6a53a7ec
Abhishek Kumar <abhi2004shek.kumar@gmail.com>
2023-08-13 22:33:44 +0530

    initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,35 @@
+
+import React from 'react';
+import logo from './logo.svg';
+
+
+const Header = () => {
+    let Links = [
+        {name:"Home", link:"/Home"},
+        {name:"Flights", link:"/Flights"},
+        {name:"Trains", link:"/Trains"},
+        {name:"Buses", link:"/Buses"},
+        {name:"Hotels", link:"/Hotels"},
+        
+    ];
+
+  return (
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4  text-white'>
+        <a href="/Home" className=" object-scale-down">
+          <img src={logo} alt="logo" height={100} width={100} />
+        </a>
+        <ul className='md:flex md:items-center '>
+            {
+                Links.map((link)=>(
+                    <li key={link.name} className='md:ml-8 font-bold text-xl'>
+                        <a href={link.link} className=' hover:text-orange-400 duration-500 hover:underline'>{link.name}</a>
+                    </li>
+                ))
+            }
+        </ul>
+        
+    </div>
+  )
+}
+
+export default Header;
